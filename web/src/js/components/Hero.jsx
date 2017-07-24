bd01266a53a0e623513bf05a72b9e1100d7414af
headz <sean@dulotech.com>
2017-07-23 23:45:22 -0400

    Adds rotating sayings in Hero

--- web/src/js/components/Hero.jsx
+++ web/src/js/components/Hero.jsx
@@ -2,24 +2,60 @@
 
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { withRouter } from 'react-router'
 
-const Hero = () => (
-  <section className="text-center hero" id="hero-area">
-    <div className="container">
-      <div className="row">
-        <div className="col-md-12">
-          <div className="intro-area">
-            <h2 className="page-title">
-              <Link to="/">pragmatism</Link>
-            </h2>
-            <h3>
-              <i>noun</i> &middot; an approach to problems and situations that is based on practical solutions.
-            </h3>
+let saying_index = -1;
+const sayings = [
+  {word: 'pragmatism', def: 'an approach to problems and situations that is based on practical solutions.'},
+  {word: 'minimalism', def: 'a style or technique that is characterized by extreme spareness and simplicity.'}
+];
+
+class Hero extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      saying: {}
+    };
+  }
+  
+  componentDidMount() {
+    this.setSaying();
+    this.props.history.listen(this.setSaying);
+  }
+  
+  setSaying = () => {
+    saying_index += 1;
+    if (saying_index > sayings.length - 1) {
+      saying_index = 0;
+    }
+    const saying = sayings[saying_index];
+    this.setState({saying});
+  };
+  
+  render() {
+    const {saying} = this.state;
+    
+    return (
+      <section className="text-center hero" id="hero-area">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="intro-area">
+                <h2 className="page-title">
+                  <Link to="/">
+                    {saying.word}
+                  </Link>
+                </h2>
+                <h3>
+                  {saying.def}
+                </h3>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  </section>
-);
+      </section>
+    );
+  }
+}
 
-export default Hero;
+export default withRouter(Hero);

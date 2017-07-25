17d51cec420f41771faa3ff93ee1e864eb1d51e3
headz <sean@dulotech.com>
2017-07-25 10:57:56 -0400

    Refactoring

--- web/src/js/front/App.jsx
+++ web/src/js/front/App.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { postsLoad } from 'front/actions/postActions';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import * as actions from 'front/actions/postActions';
 import store from 'front/store/store';
 
 import Header from 'front/components/Header';
 import Hero from 'front/components/Hero';
 import Footer from 'front/components/Footer';
 import Sidebar from 'front/components/Sidebar';
-
 import IndexPage from 'front/pages/Index';
 import AboutPage from 'front/pages/About';
 import ContactPage from 'front/pages/Contact';
+import PostPage from 'front/pages/Post';
 import LoginPage from 'front/pages/Login';
 import LogoutPage from 'front/pages/Logout';
-import PostPage from 'front/pages/Post';
 
 export default class App extends React.Component {
-  componentDidMount() {
-    store.dispatch(postsLoad());
+  constructor(props) {
+    super(props);
+    store.dispatch(actions.postsLoad());
   }
 
   render() {
--- web/src/js/index.jsx
+++ web/src/js/index.jsx
@@ -1,13 +1,8 @@
+import 'utils/fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import FrontApp from 'front/App';
 import AdminApp from 'admin/App';
-import 'whatwg-fetch';
-import Promise from 'promise-polyfill';
-
-if (!window.Promise) {
-  window.Promise = Promise;
-}
 
 if (document.location.pathname.indexOf('/admin') === 0) {
   ReactDOM.render(

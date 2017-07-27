ffcb6eb10bdad96ceabfdf03dcf45baae7ecf6a0
headz <sean@dulotech.com>
2017-07-27 13:43:47 -0400

    Animation to hero

--- web/src/js/front/components/Saying.jsx
+++ web/src/js/front/components/Saying.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Saying = ({ saying }) => (
   <div className="intro-area">
-    <h2 className="page-title">
+    <h2 className="page-title animate-pop-in">
       <Link to="/">
         {saying.word}
       </Link>
     </h2>
-    <h3>
+    <h3 className="animate-pop-in">
       {saying.def}
     </h3>
   </div>

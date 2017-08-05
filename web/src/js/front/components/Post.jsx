47beb2ba59b875f26e60ac6c9baa9172c0d98fbd
headz <sean@dulotech.com>
2017-08-05 05:47:03 -0400

    cdn usage

--- web/src/js/front/components/Post.jsx
+++ web/src/js/front/components/Post.jsx
@@ -53,7 +53,7 @@ const Post = ({ post, readMore }) => {
         {/* Image Start */}
         {post.imageURL ? (
           <div className="feature-inner">
-            <img src={post.imageURL} alt="Featured" />
+            <img src={`https://headzoo.r.worldssl.net${post.imageURL}`} alt="Featured" />
           </div>
         ) : null}
         {/* Image End */}
